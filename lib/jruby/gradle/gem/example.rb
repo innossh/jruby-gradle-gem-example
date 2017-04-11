b7f458bbcfeb940dee9c2221ba6f92ac2a266a25
innossh <innossh@users.noreply.github.com>
2017-04-11 22:46:57 +0900

    first commit

---
+++ lib/jruby/gradle/gem/example.rb
@@ -0,0 +1,11 @@
+require "jruby/gradle/gem/example/version"
+
+module Jruby
+  module Gradle
+    module Gem
+      module Example
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/jruby/gradle/gem/example/version.rb
@@ -0,0 +1,9 @@
+module Jruby
+  module Gradle
+    module Gem
+      module Example
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
